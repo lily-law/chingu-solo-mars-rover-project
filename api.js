6a6a180a83fdc2be81975279e2ce0f51fcec67d7
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-17 14:21:27 +0100

    Refactor https get request to axios get - Add axios dependancy

--- api.js
+++ api.js
@@ -1,30 +1,21 @@
-const https = require('https');
+const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
 async function getPhotoList({rover, sol, camera, page}) {
-    return new Promise((resolve, reject) => {
-        https.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}${camera !== 'any' ? '&camera='+camera : ''}&page=${page}&api_key=${process.env.NASA_API_KEY}`, (res) => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            res.on('end', async () => {
-                const json = await JSON.parse(data);
-                if (json.photos) {
-                    resolve({
-                        photos: json.photos,
-                        next: json.photos.length < 25 ? 'end' : `/api/${rover}?sol=${sol}&camera=${camera}&page=${parseInt(page)+1}`
-                    });
-                } 
-                else {
-                    reject(json); 
-                }
-            });
-        }).on("error", (err) => {
-            reject(err)
-        });
-    });
+    const response = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}${camera !== 'any' ? '&camera='+camera : ''}&page=${page}&api_key=${process.env.NASA_API_KEY}`);
+    if (response.data.errors) {
+        throw response.data.errors
+    }
+    if (response.data.photos) {
+        return {
+            photos: response.data.photos,
+            next: response.data.photos.length < 25 ? 'end' : `/api/${rover}?sol=${sol}&camera=${camera}&page=${parseInt(page)+1}`
+        }
+    }
+    else {
+        return response
+    } 
 }
 
 const roversCameras = { 
