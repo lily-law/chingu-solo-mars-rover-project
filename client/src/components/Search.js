5673ea2a7fd6b831e771a10e5cb86e30b3b73ae0
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-18 18:40:39 +0100

    Improve Search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,53 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import fetchData from './fetchData';
+import './Search.css';
 
-function Search({setPhotoData, setStatus}) {
+const handleSetSol = (e, {manifests, rover, sol, setSol}) => {
+    const photoData = manifests[rover] && manifests[rover].photos;
+    const nearest = photoData && findNextNearest(e.target.value, sol, photoData.map(p => p.sol));
+    Number.isInteger(nearest) && setSol(nearest);
+}
+
+function Search({setPhotoData, status, setStatus, manifests, rovers, cameras, rover, setRover, setPagesTotal}) {
     const [sol, setSol] = useState(0);
-    const [rover, setRover] = useState('Curiosity');
     const [camera, setCamera] = useState('any');
 
-    const cameras = {
-        "FHAZ": "Front Hazard Avoidance Camera",
-        "NAVCAM": "Navigation Camera",
-        "MAST": "Mast Camera",
-        "CHEMCAM": "Chemistry and Camera Complex",
-        "MAHLI": "Mars Hand Lens Imager",
-        "MARDI": "Mars Descent Imager",
-        "RHAZ": "Rear Hazard Avoidance Camera",
-        "PANCAM": "Panoramic Camera",
-        "MINITES": "Miniature Thermal Emission Spectrometer"
-    }
-    const roversCameras = { 
-        'Curiosity': ["FHAZ", "NAVCAM", "MAST", "CHEMCAM", "MAHLI", "MARDI", "RHAZ"],
-        'Opportunity': ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"],
-        'Spirit' : ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"]
-    };
     const handleSubmit = async () => {
-        let data = await fetchData(`/api/${rover}?sol=${sol}&camera=${camera}`, setStatus);
-        if (data && data.photos) { 
-            setPhotoData(data);
+        if (manifests[rover]) {
+            setStatus({requesting: true, message: `Fetching photo data for sol ${sol}`});
+            const pagesTotal = Math.ceil(status.totalPhotos/25);
+            setPagesTotal(pagesTotal);
+            let data = await fetchData(`/api/photos/${rover}?sol=${sol}&camera=${camera}`, setStatus, `Photo data recieved (1/${pagesTotal})`);
+            if (data && data.photos) { 
+                setPhotoData(data);
+            }
         }
     }
-   
+    const handleSetRover = e => {
+        setCamera('any');
+        setRover(e.target.value);
+        setSol(0);
+    }
+    useEffect(() => {
+        if (manifests[rover]) {
+            const photoData = manifests[rover].photos.find(photo => photo.sol === sol);
+            const totalPhotos = photoData && photoData['total_photos'];
+            totalPhotos && setStatus({done: `${rover} on Sol ${sol} has ${totalPhotos} photos ${camera !== 'any' ? 'from '+cameras[camera] : ''}`, totalPhotos});
+            handleSetSol({target: {value: sol}}, {manifests, rover, sol, setSol});
+        }
+    }, [rover, sol, setStatus, camera, cameras, manifests]);
+
     return (
-        <nav>
-            <select value={rover} onChange={e => setRover(e.target.value)}>
-                {Object.keys(roversCameras).map(rover => <option key={rover} value={rover}>{rover}</option>)}
+        <nav className="search">
+            <select className="search__rover" value={rover} onChange={handleSetRover}>
+                {rovers.map(rover => <option key={rover.name} value={rover.name}>{rover.name}</option>)}
             </select>
-            <label>
-                Sol:
-                <input type="number" value={sol} onChange={e => setSol(e.target.value)} />
+            <label className="search__sol">
+                Sol<br />
+                <input type="number" value={sol} onChange={e => handleSetSol(e, {manifests, rover, sol, setSol})} />
             </label>
-            <label>
-                Camera:
+            <label className="search__camera">
+                Camera<br />
                 <select value={camera} onChange={e => setCamera(e.target.value)}>
                     <option value='any'>any</option>
-                    {roversCameras[rover].map(cam => <option key={cam} value={cam}>{cameras[cam]}</option>)}
+                    {rovers.find(r => r.name === rover).cameras.map(cam => <option key={cam} value={cam}>{cameras[cam]}</option>)}
                 </select>
             </label>
-            <button onClick={handleSubmit}>Find Photos</button>
+            <button className="search__submit" onClick={handleSubmit}>Find Photos</button>
         </nav>
     );
 }
 
 export default Search;
+
+function findNextNearest(target, current, arr) {
+    if (arr.includes(target)) {
+        return target
+    }
+    const numbersInCorrectDirection = arr.filter(n => target > current ? n > current : n < current);
+    if (numbersInCorrectDirection.length <= 0) {
+        return target > current ? arr[arr.length - 1] : arr[0];
+    }
+    return numbersInCorrectDirection.reduce((nearest, num) => Math.abs(target - num) < Math.abs(target - nearest) ? num : nearest)
+}
