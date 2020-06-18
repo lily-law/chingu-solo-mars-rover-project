3e3965afe5375578ab1a3cf918b13c0cf457c703
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-18 02:22:55 +0100

    Improve Status component

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import Spinner from './Spinner';
+import './Status.css';
 
-export default function Status({status, photoData}) {
-  if(status && status.code && status.code >= 300) {
-    return (
-      <div>
-        {status.message}
-      </div>
-    );
+let output = [];
+
+export default function Status({status}) {
+  if (status) {
+    if (status.errors) {
+      const errorMsgs = status.errors.join(', ');
+      output = [<div key={"status__error"+errorMsgs} className="status__error">
+          {errorMsgs}
+      </div>]
+    }
+    if (status.done) {
+      const key = "status__ok"+status.done;
+      if (!output.some(elem => elem.key === key)) {
+        output.unshift(
+          <div key={key} className="status__ok">
+            {status.done}
+          </div>
+        );
+      }
+    }
+    if (status.name && status.name === "Error" && status.message) {
+      output = [<div key={"status__error"+status.message} className="status__error">
+          {status.message}
+      </div>]
+    }
+    else if (status.message) {
+      const key = "status__message"+status.message;
+      if (!output.some(elem => elem.key === key)) {
+        output.unshift(
+          <div key={key} className="status__message">
+            {status.message}
+          </div>
+        );
+      }
+    }
   }
-  return null
+  if (output.length > 3) {
+    output = output.slice(0 , 3);
+  }
+  return (
+    <div className="status">
+      {output.map(elem => elem)}
+      {status && status.requesting && <div className="status__requesting">
+        <Spinner />
+      </div>}
+    </div>
+  );
 }
