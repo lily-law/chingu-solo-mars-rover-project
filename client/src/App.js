cae00bf6b9b8cf2e9e32fea8aca9cf7889be229e
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-18 02:24:37 +0100

    Fetch manifests on first load

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import React, { useState } from 'react';
 import Search from './components/Search';
 import Gallery from './components/Gallery';
-import Status from './components/Status';
+import fetchData from './components/fetchData';
 import './App.css';
 
 function App() {
   const [photoData, setPhotoData] = useState({photos: []});
   const [status, setStatus] = useState();
+  const [manifests, setManifests] = useState({});
+
+  useEffect(() => {
+    const getManifest = async (rover) => {
+      setStatus({requesting: true, message: `Fetching manifest for ${rover}`});
+      let data = await fetchData(`/api/manifests/${rover}`, setStatus);
+      if (data && data['manifest']) { 
+          return data.manifest
+      }
+    };
+    (async () => {
+      const manis = {};
+      for (let rover of rovers) {
+        const manifest = await getManifest(rover.name);
+        if (manifest) {
+          manis[rover.name] = manifest;
+        }
+        else {
+          return
+        }
+      }
+      setManifests(manis);
+    })();
+  }, []);
+  
   return (
     <div className="App">
       <Search {...{setPhotoData, setStatus}} />
